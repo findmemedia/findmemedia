e22bfdb82437fe88c795f89fb529c17916e18b3f
Zebediah Perkins <zebmperkins@gmail.com>
2022-04-03 19:27:32 -0400

    layout frontend skeleton

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const mediaAlgorithm = require('./src/routes/mediaAlgorithm');
+const path = require('path');
 
 const app = express();
 mongoose.connect(process.env.DB_URL);
 
+app.use(express.static(path.join(__dirname, './client/build')));
+
 app.get('/algorithm', mediaAlgorithm);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 app.listen(process.env.PORT || 3000);
