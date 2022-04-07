945d6faa414e29cb64812bdd9b75cbae103f6b9d
Zebediah Perkins <zebmperkins@gmail.com>
2022-04-07 19:34:16 -0400

    clean up Search.js

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 import Card from './Card';
 
+const NO_MEDIA = "https://media.discordapp.net/attachments/192431179876532226/961744120248930404/NoMedia.png?width=600&height=900";
+
 export default function Search() {
     const [mediaResponse, setMediaResponse] = useState([]);
     const [query, setQuery] = useState(['']);
-    const NO_MEDIA = "https://media.discordapp.net/attachments/192431179876532226/961744120248930404/NoMedia.png?width=600&height=900";
-
     return (
         <div>
             <input type='text' onChange={e => setQuery(e.target.value)} />
@@ -14,9 +14,7 @@ export default function Search() {
                     .then(response => response.json())
                     .then(setMediaResponse);
             }}>Click</button>
-            {mediaResponse.map((item) => {
-                return item.poster ? <Card imgSrc={NO_MEDIA} /> : <Card imgSrc={item.poster} />
-            })}
+            {mediaResponse.map((item) => item.poster ? <Card imgSrc={item.poster} /> : <Card imgSrc={NO_MEDIA} />)}
         </div>
-    )
+    );
 }
