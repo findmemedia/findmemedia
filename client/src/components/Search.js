177b8d80f1230c5bddb18fbbd7ed0fe76a04a054
Zebediah Perkins <zebmperkins@gmail.com>
2022-04-07 19:29:52 -0400

    clean up Search.js

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,6 +4,7 @@ import Card from './Card';
 export default function Search() {
     const [mediaResponse, setMediaResponse] = useState([]);
     const [query, setQuery] = useState(['']);
+    const NO_MEDIA = "https://media.discordapp.net/attachments/192431179876532226/961744120248930404/NoMedia.png?width=600&height=900";
 
     return (
         <div>
@@ -14,8 +15,7 @@ export default function Search() {
                     .then(setMediaResponse);
             }}>Click</button>
             {mediaResponse.map((item) => {
-                if(item.poster != null) return <Card imgSrc={item.poster} />
-                return <Card imgSrc="https://media.discordapp.net/attachments/192431179876532226/961744120248930404/NoMedia.png?width=600&height=900" />
+                return item.poster ? <Card imgSrc={NO_MEDIA} /> : <Card imgSrc={item.poster} />
             })}
         </div>
     )
