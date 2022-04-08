e87bfae451d3d934844911e8d11a601d2554e037
JJ Ross <jeffreysrossjr@gmail.com>
2022-04-07 21:02:49 -0400

    begin implementing info page

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 export default function Card(props) {
     return (
         <div className="movie-card">
-            <Link to="/info"><img onError={img => img.target.src = props.NO_MEDIA} src={props.media.poster} /></Link>
+            <Link to={"/info"} state={props.media}>
+                <img onError={img => img.target.src = props.NO_MEDIA} src={props.media.poster} />
+            </Link>
         </div>
     );
 }
--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -1,7 +1,11 @@
+import { useLocation } from 'react-router-dom'
+
 export default function Info(props) {
+    const info = useLocation().state
+
     return (
         <div>
-            <h1>Info</h1>
+            <h1>{info.title}</h1>
         </div>
     )
 }
