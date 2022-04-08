6b4d4b16e2c25fd856a81e09f6931ea9f48149e3
Zebediah Perkins <zebmperkins@gmail.com>
2022-04-07 20:34:22 -0400

    add info route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main'
+import Info from './pages/Info'
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Main />} />
+                <Route path='/info' element={<Info />} />
             </Routes>
         </BrowserRouter>
     );
