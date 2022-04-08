60d6c6116d7483a6b274ef0f516bea65195a978c
JJ Ross <jeffreysrossjr@gmail.com>
2022-04-07 20:07:00 -0400

    add failsafe for dead image link

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+function addDefaultSrc(e) {
+        e.target.src = "https://media.discordapp.net/attachments/192431179876532226/961744120248930404/NoMedia.png?width=600&height=900"
+}
+
 export default function Card(props) {
     return (
         <div className="movie-card">
-            <img src={props.imgSrc} />
+            <img onError={addDefaultSrc} src={props.imgSrc} />
         </div>
     );
 }
