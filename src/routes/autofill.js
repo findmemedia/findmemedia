1b791a33f97ce9c3ae14f84f5bd870d46508d6b7
Zebediah Perkins <zebmperkins@gmail.com>
2022-04-09 01:33:09 -0400

    quick fix for limit string query

--- src/routes/autofill.js
+++ src/routes/autofill.js
@@ -2,7 +2,8 @@ const Media = require('../models/Media');
 
 module.exports = (req, res) => {
     if (!req.query.input) return res.json([]);
-    let limit = req.query.limit ? parseInt(req.query.limit) : 5;
+    let limit = parseInt(req.query.limit);
+    if (!limit) limit = 5;
     if (limit > 15) limit = 15;
     Media.find({ 'title': { '$regex': req.query.input } }, (_err, similar) => {
         res.json(similar);
