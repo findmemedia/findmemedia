10631779dcf606bfbb04b0948cc9979866f56d20
Zebediah Perkins <zebmperkins@gmail.com>
2022-04-09 14:42:22 -0400

    allow case insensitive autofill

--- src/routes/autofill.js
+++ src/routes/autofill.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
     let limit = parseInt(req.query.limit);
     if (!limit) limit = 5;
     if (limit > 15) limit = 15;
-    Media.find({ 'title': { '$regex': req.query.input } }, (_err, similar) => {
+    Media.find({ 'title': { '$regex': req.query.input, '$options': 'i' } }, (_err, similar) => {
         res.json(similar);
     }).limit(limit);
 }
