bd0c0a5ddebcaa7c679018c8e87134496907d645
JJ-Ross <jeffreysrossjr@gmail.com>
2022-07-31 19:44:51 -0400

    fixed mongodb connection bug

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 8080;
 
-mongoose.connect(`mongodb+srv://Admin:${process.env.MONGO_PWD}@find-me-media-cluster.zw2beu5.mongodb.net/?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://Admin:${process.env.MONGO_PWD}@find-me-media-cluster.zw2beu5.mongodb.net/find-me-media?retryWrites=true&w=majority`);
 
 const Media = mongoose.model(
   'media', 
@@ -21,10 +21,10 @@ app.get('/', (req, res) => {
 
 app.get('/api/test', (req, res) => {
   Media.findOne({title : '21'}, (_err, media) => {
-    if (media) {
+    if (true) {
       res.json(media);
     }
-    else res.send('failure');
+    //else res.send(_err);
   });
 });
 
