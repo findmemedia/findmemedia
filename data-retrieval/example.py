3d2f1c5af35d24d23b608d68d1587b73b9166a3e
JJ-Ross <jeffreysrossjr@gmail.com>
2022-07-31 12:24:02 -0400

    add example.py

--- data-retrieval/example.py
+++ data-retrieval/example.py
@@ -3,4 +3,4 @@ import os
 
 client = MongoClient(f"mongodb+srv://Admin:{os.environ['MONGO_PWD']}@find-me-media-cluster.zw2beu5.mongodb.net/?retryWrites=true&w=majority")
 
-m = client['find-me-media'].media.find_one({'title' : 'Fever Pitch'})
+client['find-me-media'].media.insert_one({'title' : '21'})
