a671e711bd0c60be2ca8acb0ed2efad5d427f4e0
JJ-Ross <jeffreysrossjr@gmail.com>
2022-07-31 13:56:18 -0400

    start implementing express mongo stuff

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const app = express();
 const port = 8080;
+
+mongoose.connect(`mongodb+srv://Admin:${process.env.MONGO_PWD}@find-me-media-cluster.zw2beu5.mongodb.net/?retryWrites=true&w=majority`);
+
+const Media = mongoose.model(
+  'media', 
+  new mongoose.Schema({
+      title: String
+  })
+);
+
 
 app.use(cors());
 
@@ -10,7 +21,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/test', (req, res) => {
-  res.send('test');
+  res.json(Media.findOne({title : '21'}, (_err, media) => {
+    if (media) {
+      res.json(media);
+    }
+    else res.send('failure');
+  }));
 });
 
 app.listen(port, () => {
