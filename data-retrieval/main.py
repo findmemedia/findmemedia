15168e4f042c88d41c1a31df69adfbd7c03c5f38
JJ-Ross <jeffreysrossjr@gmail.com>
2022-08-02 20:53:04 -0400

    check for pre-existing documents in database

--- data-retrieval/main.py
+++ data-retrieval/main.py
@@ -6,12 +6,12 @@ import os
 
 client = MongoClient(f"mongodb+srv://Admin:{os.environ['MONGO_PWD']}@find-me-media-cluster.zw2beu5.mongodb.net/?retryWrites=true&w=majority")
 
-
 for page in range(1, 501):
     for movie in get_page('movie', page)['results']:
-        client['find-me-media'].test.insert_one({
-            'details': get_media('movie', movie['id'], 'details'),
-            'credits': get_media('movie', movie['id'], 'credits'),
-            'keywords': get_media('movie', movie['id'], 'keywords'),
-            'providers': get_media('movie', movie['id'], 'watch/providers')
-        })
+        if client['find-me-media']['test'].count_documents({ 'details.title': movie['title'] }) == 0:
+            client['find-me-media']['test'].insert_one({
+                'details': get_media('movie', movie['id'], 'details'),
+                'credits': get_media('movie', movie['id'], 'credits'),
+                'keywords': get_media('movie', movie['id'], 'keywords'),
+                'providers': get_media('movie', movie['id'], 'watch/providers')
+            })
